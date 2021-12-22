c9fa8a56f45d0ef6324f1949f59e84c9a7e1e5e7
shuhul <shuhulmujoo@gmail.com>
2021-12-22 14:15:30 -0800

    Normalized image data for training from DaimlerMono

--- src/newfiles/DataHandler.py
+++ src/newfiles/DataHandler.py
@@ -4,22 +4,41 @@ import matplotlib.pyplot as plt
 
 def getData():
     imgs = []
-    anots = []
-    os.chdir('../../data/PennFudan/')
-    for file in os.listdir('Annotation'):
-        with open('Annotation/' + file, 'r') as f:
-            lines = f.readlines()
-            name = lines[4][28:29]
-            anots.append(name)
-            # print(name)
-            # image = cv2.imread(name)
-            # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            # image = cv2.resize(image, (256, 256), interpolation = cv2.INTER_AREA)
-            # imgs.append(image)
-    os.chdir('../../data/Training/Annotation')
-    for i in range(len(anots)):
-        with open(f'img{i}.txt', 'w') as f:
-            f.write(f'Number of people: {anots[i]}')
+    # anots = []
+    # os.chdir('../../data/DaimlerMono/ped_examples')
+    # for file in os.listdir():
+    #     image = cv2.imread(file)
+    #
+    #     image = cv2.resize(image, (256, 256), interpolation = cv2.INTER_CUBIC)
+    #     imgs.append(image)
+    #     # with open('ped_examples/' + file, 'r') as f:
+    #     #     # lines = f.readlines()
+    #     #     # name = lines[4][28:29]
+    #     #     # anots.append(name)
+    #     #     # print(name)
+    #     #     image = cv2.imread(name)
+    #     #     # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #     #     image = cv2.resize(image, (256, 256), interpolation = cv2.INTER_AREA)
+    #     #     imgs.append(image)
+    # #
+    # os.chdir('../../../data/Training/DaimlerMono/Images/Grayscale')
+    # for i in range(len(imgs)):
+    #     if((i%10) == 0):
+    #         cv2.imwrite(f'img{int((i/10))}.png', imgs[i])
+
+
+        # with open(f'img{i}.txt', 'w') as f:
+        #     f.write(f'Number of people: {anots[i]}')
+    os.chdir('../../data/Training/DaimlerMono/Annotation')
+    for i in range(201):
+        with open(f'img{i}', 'w') as f:
+            n = 1
+            if(i > 100):
+                n = 0
+            f.write(f'Number of people: {n}')
+
+    # print(len(imgs))
+    # print(imgs[0].shape)
     # plt.imshow(imgs[0])
     # plt.show()
     pass
